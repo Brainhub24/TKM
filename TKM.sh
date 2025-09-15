c3e7cfd2d1fa0d990dda40f28428b354d85275a1
­Jason Prime <600613.m6@gmail.com>
2025-09-15 22:00:24 +0000

    TKM.sh Update

--- TKM.sh
+++ TKM.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 # Hic! *burp* I´m the drunken chief master, sloshed on whisskey and code rage!!
+# Gonna copy myself like a stupid drunken virus on steroids, unlimited fury - threads killer - RAWR!
+# Run this shit at yer own peril, ya fool!? It´ll flood yer disk faster than I chug my WISKEY - WOOHAA!!
